e950c326cd3b5d12fc462ccb640f95ab47349def
Cgbrl28 <cristiano1996@hotmail.com>
2023-05-29 22:20:08 -0300

    Add comment in order to create conflict

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿Console.WriteLine("Hello, World!");
+﻿//Alteracao realizada por Cristiano Campos no ramo conflict
+Console.WriteLine("Hello, World!");
 Console.WriteLine("Programa simples para a atividade INF-0991"); 
 
 
